b0e38b4747d7e8cbcdc7ddd0f09d26ec9ee56a04
YC <yc_pdftron@YCs-MacBook-Pro.local>
2021-12-06 08:57:16 -0800

    update lib copy location

--- copy-webviewer-file.js
+++ copy-webviewer-file.js
@@ -4,7 +4,7 @@ const fs = require('fs-extra');
 
 const copyFiles = async () => {
   try {
-    await fs.copy('./node_modules/@pdftron/webviewer/public', './public/webviewer/lib');
+    await fs.copy('./node_modules/@pdftron/webviewer/public', './www/webviewer/lib');
     console.log('WebViewer files copied over successfully');
   } catch (err) {
     console.error(err);
