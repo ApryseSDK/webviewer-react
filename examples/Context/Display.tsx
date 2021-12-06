2f707aea7cf8e5aad79cde80ff2d45fdedc6fde8
Yixiao Chen <ychen239@uottawa.ca>
2021-12-06 01:49:47 -0800

    update examples

--- examples/Context/Display.tsx
+++ examples/Context/Display.tsx
@@ -1,24 +1,39 @@
-import React, { useState, useEffect, useContext } from 'react'
-import { DocumentViewerContext } from '../../src/components/Context'
-import { TContextState } from '../../src/components/Context'
+import React, { useState, useEffect } from 'react'
+import useInstance from '../../src/hooks/useInstances'
 
-const docs = ['PDFTRON_about.pdf', '1.pdf', '2.pdf', '3.pdf']
+const docs = [
+  'PDFTRON_about.pdf',
+  '1.pdf',
+  '2.pdf',
+  '3.pdf',
+  '1.png',
+  '2.png',
+  '3.png',
+  '4.png',
+  '5.png',
+  '6.png',
+  '7.png',
+]
 
 function Display() {
-  const {instances, addInstance} = useContext(DocumentViewerContext)
+    
+  const { instances, addInstance } = useInstance()
 
   const [elements, setElements] = useState([])
 
   function appendNewDocumentViewer() {
-    console.log('insinsins - ', instances)
-    const rnd = Math.floor(Math.random() * 4)
+    const rnd = Math.floor(Math.random() * docs.length)
     const UID = Date.now().toString(16)
-    const el = document.createElement('div')
     const options = {
       initialDoc: `files/${docs[rnd]}`,
       UID: UID,
     }
     setElements([...elements, options])
+  }
+
+  function LoadRandomDocument(key) {
+    const rnd = Math.floor(Math.random() * docs.length)
+    instances[key].UI.loadDocument(`http://127.0.0.1:8000/files/${docs[rnd]}`)
   }
 
   useEffect(() => {
@@ -31,7 +46,9 @@ function Display() {
     }
   }, [elements])
 
-  function removeDocumentViewer() {}
+  useEffect(() => {
+    console.log('List of instances - ', instances)
+  }, [instances])
 
   return (
     <>
@@ -42,7 +59,18 @@ function Display() {
       </div>
       <div>
         {elements.map(el => (
-          <div id={el.UID} key={el.UID} />
+          <div
+            key={el.UID}
+            style={{
+              display: 'flex',
+              alignItems: 'center',
+              borderTop: '2px solid',
+            }}>
+            <button onClick={() => LoadRandomDocument(el.UID)}>
+              Load Random Document With This Instance
+            </button>
+            <div id={el.UID} style={{ height: '300px', width: '100%' }} />
+          </div>
         ))}
       </div>
     </>
