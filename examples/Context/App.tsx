ead4fd163fdd685d3ace13aae75b5111fa3e68e1
Yixiao Chen <ychen239@uottawa.ca>
2021-12-07 22:17:49 -0800

    remove unused import

--- examples/Context/App.tsx
+++ examples/Context/App.tsx
@@ -1,11 +1,9 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import { DocumentViewerProvider } from '../../lib'
 import Body from './Body'
 import '../_css/App.css'
 
 const App = () => {
-
-  useEffect
 
   return (
     <DocumentViewerProvider>
