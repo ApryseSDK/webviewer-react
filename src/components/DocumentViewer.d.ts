89090e2987ed0ff2b6e8a34fef8b8974fe310927
Yixiao Chen <ychen239@uottawa.ca>
2021-12-07 17:05:30 -0800

    remove d.ts

--- src/components/DocumentViewer.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference types="react" />
-export declare type TProps = {
-    docUrl: string;
-    UID: string;
-};
-declare function DocumentViewer({ docUrl, UID }: TProps): JSX.Element;
-export default DocumentViewer;
