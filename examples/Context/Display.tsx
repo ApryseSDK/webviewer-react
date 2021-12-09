50002ec660439b4538bfd3b17bb8801f767a3880
Yixiao Chen <ychen239@uottawa.ca>
2021-12-08 16:43:57 -0800

    update example

--- examples/Context/Display.tsx
+++ examples/Context/Display.tsx
@@ -1,16 +1,29 @@
 import React, { useState, useEffect, useRef } from 'react'
-import useInstance from '../../lib'
-import { DocumentViewerSimpleDisplay } from '../../lib'
-import { getRandomLocalDocUrl, getInitialWvOptions, getRandomInt } from './utils'
-
+import useInstance, {
+  DocumentViewer,
+  DocumentViewerSimpleDisplay,
+} from '../../lib'
+import { getInitialWvOptions } from './utils'
+import Modal from './Modal'
 
 function Display() {
-  const { instance } = useInstance()
+  const { instance, setInstance } = useInstance()
   const [open, setOpen] = useState(false)
+  const [fullUi, setSFullUi] = useState(false)
   const ref = useRef(null)
 
   function toggleModal() {
     setOpen(!open)
+  }
+
+  function simpleDisplay() {
+    setInstance(undefined)
+    setSFullUi(false)
+  }
+
+  function showFullUi() {
+    setInstance(undefined)
+    setSFullUi(true)
   }
 
   useEffect(() => {
@@ -20,88 +33,23 @@ function Display() {
   return (
     <>
       <div style={{ marginBottom: '10px' }}>
+        <button onClick={simpleDisplay} className='buttons'>
+          Simple Display
+        </button>
+        <button onClick={showFullUi} className='buttons'>
+          Full UI
+        </button>
         <button onClick={toggleModal} className='buttons'>
-          Toggle Modal
+          Controls Inside Modal
         </button>
       </div>
-      {open && <Modal toggleModal={toggleModal}/>}
-      <DocumentViewerSimpleDisplay ref={ref} {...getInitialWvOptions()}/>
+      {open && <Modal toggleModal={toggleModal} />}
+      {fullUi ? (
+        <DocumentViewer ref={ref} {...getInitialWvOptions()} />
+      ) : (
+        <DocumentViewerSimpleDisplay ref={ref} {...getInitialWvOptions()} />
+      )}
     </>
-  )
-}
-
-function Modal ({toggleModal}) {
-  const { instance } = useInstance()
-
-  function zoomIn() {
-    instance.UI.setZoomLevel(instance.UI.getZoomLevel() + 0.2)
-  }
-
-  function ReplaceDocument() {
-    instance.UI.loadDocument(getRandomLocalDocUrl())
-  }
-
-  function addRandomAnnotation() {
-    const rectangleAnnot = new instance.Core.Annotations.RectangleAnnotation()
-    rectangleAnnot.PageNumber = 1
-    rectangleAnnot.X = getRandomInt(500)
-    rectangleAnnot.Y = getRandomInt(800)
-    rectangleAnnot.Width = getRandomInt(200)
-    rectangleAnnot.Height = getRandomInt(200)
-    rectangleAnnot.FillColor = new instance.Core.Annotations.Color(
-      getRandomInt(255),
-      getRandomInt(255),
-      getRandomInt(255)
-    )
-    rectangleAnnot.Author = 'Test User'
-    rectangleAnnot.setContents('Comment on this rectangle')
-    const manager = instance.Core.annotationManager
-    manager.addAnnotation(rectangleAnnot)
-    manager.redrawAnnotation(rectangleAnnot)
-  }
-
-  function toggleToolsHeader() {
-    const el = ['toolsHeader']
-    instance.UI.enableElements(el)
-  }
-
-  return (
-    <div
-      style={{
-        display: 'flex',
-        position: 'absolute',
-        top: '50%',
-        left: '50%',
-        transform: 'translate(-50%, -50%)',
-        width: 'auto',
-        minWidth:'540px',
-        height: '400px',
-        boxShadow: '0 0 10px',
-        backgroundColor: '#FFFFFFF0',
-      }}>
-        <div className='hover' style={{position: 'absolute', top:'10px', right:'10px', fontSize:'24px'}} onClick={toggleModal}>X</div>
-      <div
-        style={{
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-          justifyContent:'center'
-        }}>
-        <div
-          style={{ flexDirection: 'row', display: 'flex', marginTop: '5px' }}>
-          <button onClick={ReplaceDocument} className='buttons'>
-            Change Document
-          </button>
-          <button onClick={zoomIn} className='buttons'>
-            Zoom In
-          </button>
-          <button onClick={addRandomAnnotation} className='buttons'>
-            Add Annotation
-          </button>
-          <button onClick={toggleToolsHeader} className='buttons'>Enable Tools Header</button>
-        </div>
-      </div>
-    </div>
   )
 }
 
--- examples/Context/utils.ts
+++ examples/Context/utils.ts
@@ -10,36 +10,60 @@ export const docs = [
     '5.png',
     '6.png',
     '7.png',
-  ]
-  //https://pdftron-ychen02.web.app
-  
-  export function getRandomLocalDocUrl() {
+]
+//https://pdftron-ychen02.web.app
+
+export function getRandomLocalDocUrl() {
     const rnd = Math.floor(Math.random() * docs.length)
     // TODO: setup env
     return `http://127.0.0.1:8000/files/${docs[rnd]}`
-  }
-  
-  export function getInitialWvOptions() {
-    return {initialDoc: getRandomLocalDocUrl(), path: 'http://127.0.0.1:8000/webviewer/lib'}
-  }
-  
-  export function getRandomInt(max) {
-    return Math.floor(Math.random() * max)
-  }
+}
 
-  export function buildRandomRectAnnot (instance) {
+export function getInitialWvOptions() {
+    return { initialDoc: getRandomLocalDocUrl(), path: 'http://127.0.0.1:8000/webviewer/lib' }
+}
+
+export function getRandomInt(max) {
+    return Math.floor(Math.random() * max)
+}
+
+export function buildRandomRectAnnot(instance) {
     const _annot = new instance.Core.Annotations.RectangleAnnotation()
     _annot.PageNumber = 1
     _annot.X = getRandomInt(500)
-    _annot.Y = getRandomInt(800)
-    _annot.Width = getRandomInt(200)
-    _annot.Height = getRandomInt(200)
+    _annot.Y = getRandomInt(500)
+    _annot.Width = getRandomInt(100)
+    _annot.Height = getRandomInt(100)
     _annot.FillColor = new instance.Core.Annotations.Color(
-      getRandomInt(255),
-      getRandomInt(255),
-      getRandomInt(255)
+        getRandomInt(255),
+        getRandomInt(255),
+        getRandomInt(255)
     )
     _annot.Author = 'Test User'
     _annot.setContents('Comment on this rectangle')
     return _annot
-  }
+}
+
+export const modalStyle = {
+    display: 'flex',
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 'auto',
+    minWidth: '540px',
+    height: '200px',
+    boxShadow: '0 0 10px',
+    backgroundColor: '#FFFFFFF0'
+}
+
+export const closeStyle = { position: 'absolute', top: '10px', right: '10px', fontSize: '24px' }
+
+export const modalWrapper = {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center'
+}
+
+export const rowStyle = { flexDirection: 'row', display: 'flex', marginTop: '5px', padding:'10px'}
