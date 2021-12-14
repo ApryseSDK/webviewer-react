c37e911a6abf069f7022782828448b3c65c6c286
Yixiao Chen <ychen239@uottawa.ca>
2021-12-14 14:53:42 -0800

    fix typo and file structure

--- src/components/DocumentViewerSimpleDisplay.tsx
+++ src/components/DocumentViewerSimpleDisplay.tsx
@@ -1,7 +1,6 @@
-// @ts-ignore
 import React from 'react'
 import DocumentViewer from './DocumentViewer'
-import { DISABLED_ELEMENTS } from '../utils'
+import { DISABLED_ELEMENTS } from '../utils/utils'
 import type { TProps, TRef } from './DocumentViewer'
 
 const DocumentViewerSimpleDisplay = React.forwardRef<TRef, TProps>((props, ref) => {
