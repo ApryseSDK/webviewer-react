c7d1a3f31e3e6a92f6b988ee1f6f00772ea6b9d5
Yixiao Chen <ychen239@uottawa.ca>
2021-12-14 14:55:40 -0800

    simplify return

--- src/context/useInstance.ts
+++ src/context/useInstance.ts
@@ -2,7 +2,6 @@ import { DocumentViewerContext } from './Context'
 import { useContext } from 'react';
 import { TContextState } from './Context';
 
-export default function useInstance(): TContextState{
-  const { instance, setInstance } = useContext(DocumentViewerContext);
-  return { instance, setInstance }
+export default function useInstance(): TContextState {
+  return useContext(DocumentViewerContext)
 }
