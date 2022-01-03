03bdb7c05cbc39f3170b216af84de3d59e6b3048
Yixiao Chen <ychen239@uottawa.ca>
2022-01-03 08:51:31 -0800

    boolean props for simple display

--- src/components/DocumentViewerSimpleDisplay.tsx
+++ src/components/DocumentViewerSimpleDisplay.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import DocumentViewer from './DocumentViewer';
-import { DISABLED_ELEMENTS } from '../utils/utils';
+import { DISABLED_ELEMENTS } from '../CONSTANTS';
 import type { TProps, TRef } from './DocumentViewer';
 
 const DocumentViewerSimpleDisplay = React.forwardRef<TRef, TProps>((props, ref) => {
-  return <DocumentViewer disabledElements={DISABLED_ELEMENTS} {...props} ref={ref} />;
+  return <DocumentViewer isSimpleDisplay disabledElements={DISABLED_ELEMENTS} {...props} ref={ref} />;
 });
 
 export default DocumentViewerSimpleDisplay;
